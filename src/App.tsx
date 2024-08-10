ff6b0ed5082e27a584dad139f80bb663bdddeec2
Polkhanova Veronika <polkhanova@aheadworks.com>
2024-08-10 10:53:27 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import Chat from "./pages/Chat";
+import Chats from "./pages/Chats";
+import Login from "./pages/Login";
+import Loading from "./pages/Loading";
+import Header from "./components/Header";
+import ProtectedRoute from "./utilits/ProtectedRoute";
 
 function App() {
+  const { isLoading }: any = useAuth0();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Chats />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: ":robotId",
+      element: (
+        <ProtectedRoute>
+          <Chat />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+  ]);
+
+  if (isLoading) return <Loading />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
   );
 }
 
